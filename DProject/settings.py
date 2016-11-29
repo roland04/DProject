69f6fbcc0254743afea7e2fd4117782d8e6fa2b6
Mikel <mikel.martin@edooca.com>
2016-11-29 17:54:32 +0100

    log system base

--- DProject/settings.py
+++ DProject/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'DProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -101,3 +101,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+os.path.join(BASE_DIR, "static"),
+]
+
+LOGIN_REDIRECT_URL = '/' # It means home view
--- DProject/urls.py
+++ DProject/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth import views
+from log.forms import LoginForm
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'', include('log.urls')),
+    url(r'^login/$', views.login, {'template_name': 'login.html', 'authentication_form': LoginForm}),
+    url(r'^logout/$', views.logout, {'next_page': '/login'}),
 ]
