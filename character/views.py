f22d0ef91bc8cf771fe774bc53aa20c79444005a
roland <mikelo.fmc@gmail.com>
2017-01-18 12:14:12 +0100

    template character changes

--- character/views.py
+++ character/views.py
@@ -7,4 +7,4 @@ from .models import Character
 @login_required(login_url="login/")
 def detail(request, character_id):
     character = get_object_or_404(Character, pk=character_id)
-    return render(request, "character.html", {'character': character})
+    return render(request, "character.html", {'character': character, 'hitpointspercent' : character.current_hitpoints * 100 / character.max_hitpoints })
